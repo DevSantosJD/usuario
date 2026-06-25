9337510cbd5a8a37a2219b32e13ad1821a7504fe
Jardel Santos <jardel.santos.23@outlook.com>
2026-06-25 18:04:15 -0300

    inclusão de metodo put de usuario

--- src/main/java/com/javanauta/usuario/buisness/UsuarioService.java
+++ src/main/java/com/javanauta/usuario/buisness/UsuarioService.java
@@ -4,7 +4,9 @@ import com.javanauta.usuario.buisness.converter.UsuarioConverter;
 import com.javanauta.usuario.buisness.dto.UsuarioDTO;
 import com.javanauta.usuario.infrastructure.entity.Usuario;
 import com.javanauta.usuario.infrastructure.exceptions.ConflictException;
+import com.javanauta.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.javanauta.usuario.infrastructure.repository.UsuarioRepository;
+import com.javanauta.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
 
     // Salva Usuario
@@ -58,4 +61,29 @@ public class UsuarioService {
     public void deleteUsuarioPorEmial(String email){
         usuarioRepository.deleteByEmail(email);
     }
+
+    /*
+    O metodo abaixo possui a responsabilidade de atualizaro os dados do usuario da seguinte forma:
+        - Recebe o dto e verifica os dados passados nele pelo usuario.
+        - O dado recebido no dto será atualizado, o dado que nao for passado, ele pega da entity,
+            dessa forma, atualizamos o que foi enviado, e mantemos os outros dados.
+     */
+    public UsuarioDTO atualizarDadosUsuario(String token, UsuarioDTO dto){
+        //aqui chamaos o metodo extractUsername() para extrairmos o email do token
+        String email = jwtUtil.extractUsername(token.substring(7));
+
+        // Criptografa a senha novamente caso passada no dto, se nao passada, mantem como esta no banco
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email)
+                .orElseThrow(()-> new ResourceNotFoundException("Email não encontrado"));
+
+        //Este trecho tem a responsabilidade de mesclar os dados passados no dto com os dados da entity
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+
+        //Aqui salvamos o usuario no banco e convertemos para dto
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
+
 }
--- src/main/java/com/javanauta/usuario/controller/UsuarioController.java
+++ src/main/java/com/javanauta/usuario/controller/UsuarioController.java
@@ -43,4 +43,10 @@ public class UsuarioController {
         usuarioService.deleteUsuarioPorEmial(email);;
         return ResponseEntity.ok().build();
     }
+
+    @PutMapping
+    public ResponseEntity<UsuarioDTO> atualizaDadoUsuario(@RequestBody UsuarioDTO dto,
+                                                           @RequestHeader("Authorization") String token ){
+        return ResponseEntity.ok(usuarioService.atualizarDadosUsuario(token, dto));
+    }
 }
